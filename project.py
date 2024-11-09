b6cedaf7834867c559b3aa6cd11425bad60b9eee
Zenkil <kyler.padovani@gmail.com>
2024-11-08 23:45:46 -0800

    uploading to swap to mac test

--- project.py
+++
@@ -1,3 +0,0 @@
-print('I am showing you how to push assignments up to GitHub!')
-
-print ('Making Changes!')
---
+++ python_basics.py
@@ -0,0 +1,10 @@
+Variables are used to store data. They can be changed.
+1. name = "John"
+2. age = 25
+3. height = 5.9
+
+
+Data types:
+1 age = 25 #Interger (int): Whole numbers. `10`, `-5`, `0`
+2 height = 5.09 #Float (float): Numbers with decimal points. `3.14`, `-2.5`
+3 name = "Alive" # String Sequence of characters (str): Text. `"Hello"`, `"World"`
