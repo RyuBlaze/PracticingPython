5f88c4a40f34a86dac9a71c5496e8e5cbfeecf71
ZaneRyu <zaneryu@MacBook-Pro.local>
2024-11-09 00:14:09 -0800

    SwappinPC's test

--- python_basics.py
+++ python_basics.py
@@ -8,3 +8,6 @@ Data types:
 1 age = 25 #Interger (int): Whole numbers. `10`, `-5`, `0`
 2 height = 5.09 #Float (float): Numbers with decimal points. `3.14`, `-2.5`
 3 name = "Alive" # String Sequence of characters (str): Text. `"Hello"`, `"World"`
+
+
+writing this to test if it cleanly swaps over
